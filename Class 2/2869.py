fdd911be7138c5269bc1cfad9472f9b07a48d111
hyuncheul <byp0352@gmail.com>
2025-05-11 13:26:36 +0900

    25.05.11

--- Class 2/2869.py
+++ Class 2/2869.py
@@ -1,12 +1,5 @@
 A, B, V = map(int, input().split())
 
-day = 0
-
-
-if V % (A-B) == 0 :
-    day = V // (A-B) - 1 
-else: day = V // (A-B) + 1
-
-
+day = (V - B + (A - B - 1)) // (A - B)
 
 print(day)
---
+++ Class 2/28702.py
@@ -0,0 +1,26 @@
+# Fizz 3의배수
+# Buzz 5의배수
+# FizzBuzz 15의 배수 
+
+A = input()
+B = input()
+C = input()
+
+if A not in ["Fizz", "Buzz", "FizzBuzz"]:
+    A = int(A)
+    D = A + 3
+elif B not in ["Fizz", "Buzz", "FizzBuzz"]:
+    B = int(B)
+    D = B + 2
+elif C not in ["Fizz", "Buzz", "FizzBuzz"]:
+    C = int(C)
+    D = C + 1
+
+if D % 15 == 0:
+    D = 'FizzBuzz'
+elif D % 5 == 0:
+    D = 'Buzz'
+elif D % 3 == 0:
+    D = 'Fizz'
+
+print(D)
